cc17dd44c45854c5c0d062babc6755a0e7e53fd7
Inna-Shehovtsova <innashehovtsova@gmail.com>
2024-08-19 13:49:33 +0300

    fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,38 +1,28 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-import path from "path";
-import { fileURLToPath } from "url";
-
 import tseslint from "typescript-eslint";
-//import jest from "eslint-plugin-jest";
-// mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-export default tseslint.config(
+
+
+//import { FlatCompat } from "@eslint/eslintrc";
+//import path from "path";
+//import { fileURLToPath } from "url";
+
+export default [
+  
+  {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  {
-    files: ["**/*.ts"],
+  {files: ["**/*.{js,mjs,cjs,ts}"],
     rules: {
-      //"no-unused-vars":"warn",
+      "no-unused-vars":"warn",
       "@typescript-eslint/no-unused-vars": "warn",
       "@typescript-eslint/no-unsafe-function-type": "warn",
     },
   },
-  {
-    ignores: ["coverage/*", "dist/*", "*config*"],
-  },
-  //{
-  //  files: ["**/*.test.ts"],
-  //  ...jest.configs["flat/recommended"],
-  //   rules: {
-  //    ...jest.configs["flat/recommended"].rules,
-  //    "jest/prefer-expect-assertions": "off"
-  //  },
-  // },
-);
+  {files:["webpack.config.js", "jest.config.js"], 
+    rules:{
+      "@typescript-eslint/no-require-imports":"off",
+      "no-undef":"off"
+    }
+  }
+];
